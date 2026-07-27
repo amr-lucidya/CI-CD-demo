e71839c971226f0a03cbaa9b82907d3ba95b6b74
Amr Abdelhady <asayed@lucidya.com>
2026-07-27 13:11:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
   return (
     <div className="app">
       <header className="app__header">
-        <h1>🚀 Task</h1>
+        <h1>🚀 Task Flow</h1>
         <p className="app__subtitle">A tiny app for a CI/CD demo</p>
       </header>
 
